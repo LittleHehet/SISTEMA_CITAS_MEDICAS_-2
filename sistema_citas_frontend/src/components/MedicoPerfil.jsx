9b6c9c6a8ddf587bbc2eed7cd5aaf968d785c8e0
Kendra <kendra.artavia.caballero@gmail.com>
2025-05-20 19:55:40 -0600

    Quitar coma

--- sistema_citas_frontend/src/components/MedicoPerfil.jsx
+++ sistema_citas_frontend/src/components/MedicoPerfil.jsx
@@ -97,7 +97,6 @@ function MedicoPerfil() {
             if (manana && tarde) return `${manana},${tarde}`;
             if (manana) return `${manana},`;
             if (tarde) return `,${tarde}`;
-            return ","; // Día sin horario, pero con coma para cumplir con el formato
         }).join(';');
 
         formData.append('horario', horarioFinal);
