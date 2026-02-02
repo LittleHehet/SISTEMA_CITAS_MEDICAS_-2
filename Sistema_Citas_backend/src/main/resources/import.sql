a6ef63562d9a4bd4063ad1b40b91205c42e0b3b6
Kendra Artavia Caballero <kendra.artavia.caballero@gmail.com>
2026-02-02 15:39:33 -0600

    Import.sql

---
+++ Sistema_Citas_backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+-- Localidades
+INSERT INTO localidad (localidad_nombre) VALUES ('San Jose'), ('Alajuela'), ('Cartago'), ('Heredia'), ('Guanacaste'), ('Puntarenas'), ('Limon');
+
+-- Especialidades
+INSERT INTO especialidad (especialidad_nombre) VALUES ('Alergologia'), ('Anestesiologia y reanimacion'), ('Aparato digestivo'), ('Cardiologia'), ('Endocrinologia y nutricion'), ('Geriatria'), ('Hematologia y hemoterapia'), ('Medicina de la educacion fisica y del deporte'), ('Medicina espacial'), ('Medicina intensiva'), ('Medicina interna'), ('Medicina legal y forense'), ('Medicina preventiva y salud publica'), ('Medicina del trabajo'), ('Nefrologia'), ('Neumologia'), ('Neurologia'), ('Neurofisiologia Clinica'), ('Oncologia medica'), ('Oncologia radioterapica'), ('Pediatria'), ('Psiquiatria'), ('Rehabilitacion'), ('Reumatologia'), ('Medicina familiar y comunitaria'), ('Biomedicina');
+
+-- Usuarios por defecto
+INSERT INTO usuarios (cedula, nombre, apellido, clave, perfil) VALUES
+                                                                   (111111111, 'Administrador', 'Administrador', '$2b$12$a0NvQ8Gin2hFGcRm4TTlquNpeMwuYXDiw5RzJBPwwsDrLdHvGQnI.', 'ROLE_ADMINISTRADOR'),
+                                                                   (000000000, 'Anonimo', 'Anonimo', '$2b$12$a0NvQ8Gin2hFGcRm4TTlquNpeMwuYXDiw5RzJBPwwsDrLdHvGQnI.', 'ROLE_ANONIMO');
