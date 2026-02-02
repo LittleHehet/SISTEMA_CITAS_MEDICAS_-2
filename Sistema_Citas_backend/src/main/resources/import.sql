478327190a4a9b923c996f26756dbbe6012ba2da
Kendra Artavia Caballero <kendra.artavia.caballero@gmail.com>
2026-02-02 17:22:06 -0600

    Update import.sql

--- Sistema_Citas_backend/src/main/resources/import.sql
+++ Sistema_Citas_backend/src/main/resources/import.sql
@@ -19,8 +19,11 @@ INSERT INTO especialidad (especialidad_nombre) VALUES
                                                    ('Rehabilitacion'), ('Reumatologia'), ('Medicina familiar y comunitaria'), ('Biomedicina')
     ON CONFLICT (especialidad_nombre) DO NOTHING;
 
+DELETE FROM usuarios WHERE cedula IN (111111111, 0);
+
+
 -- Usuarios por defecto (password: Hola213)
 INSERT INTO usuarios (cedula, nombre, apellido, clave, perfil) VALUES
-                                                                   (111111111, 'Administrador', 'Administrador', '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36YvY0oG5c1yV5qW8kZpG9y', 'ROLE_ADMINISTRADOR'),
-                                                                   (0,         'Anonimo',       'Anonimo',       '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36YvY0oG5c1yV5qW8kZpG9y', 'ROLE_ANONIMO')
+                                                                   (111111111, 'Administrador', 'Administrador', '$2a$10$sx8wxSC8/eXOy9lSOegE9.HNaGi/sZFJCV6zgbksuEVU736lp34yW', 'ROLE_ADMINISTRADOR'),
+                                                                   (0,         'Anonimo',       'Anonimo',       '$2a$10$sx8wxSC8/eXOy9lSOegE9.HNaGi/sZFJCV6zgbksuEVU736lp34yW', 'ROLE_ANONIMO')
     ON CONFLICT (cedula) DO NOTHING;
