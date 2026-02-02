d08ceadb42f36caff4d9a3f68e24b5903454bb80
Kendra Artavia Caballero <kendra.artavia.caballero@gmail.com>
2026-02-02 14:55:26 -0600

    Update SecurityConfig.java

--- Sistema_Citas_backend/src/main/java/com/example/sistema_citas/security/SecurityConfig.java
+++ Sistema_Citas_backend/src/main/java/com/example/sistema_citas/security/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfig.setAllowedOrigins(List.of("http://localhost:5173")); // dirección del frontend
+                    corsConfig.setAllowedOrigins(List.of("http://localhost:5173",
+                            "https://tu-frontend-en-render.onrender.com")); // dirección del frontend
                     corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfig.setAllowedHeaders(List.of("*"));
                     corsConfig.setAllowCredentials(true);
